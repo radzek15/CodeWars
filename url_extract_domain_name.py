9c3355caa67dd26be7355725916b1d86e6cef9de
radzek15 <radzek15@wp.pl>
2023-04-17 15:51:41 +0200

    domain name extract from url solution

---
+++ url_extract_domain_name.py
@@ -0,0 +1,12 @@
+"""Write a function that when given a URL as a string, parses out just the domain name and returns it as a string.
+For example:
+
+* url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+* url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+* url = "https://www.cnet.com"                -> domain name = cnet" """
+
+
+import re
+
+def domain_name(url):
+    return next(i for i in re.split(r'http://|https://|www|\.', url) if i)
