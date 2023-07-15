5fd21acd1f2d581573de3da7847268df9d45ebf0
radzek15 <radzek15@wp.pl>
2023-07-15 12:45:45 +0200

    some sql solutions added

---
+++ SQL/8kyu/Find all active students.sql
@@ -0,0 +1,10 @@
+/*
+ Create a simple SELECT query to display student information of all ACTIVE students.
+
+TABLE STRUCTURE:
+students
+Id (integer) 	FirstName (text) 	LastName (text) 	IsActive (boolean)
+ */
+
+SELECT * FROM students
+WHERE isActive = 1;
